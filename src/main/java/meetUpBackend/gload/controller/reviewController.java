dfaa9cdeeeededd2fbbdea33d83b2062b6af1021
Jeonghoo An <austinan@naver.com>
2022-10-29 00:17:55 +0900

    settings

---
+++ src/main/java/meetUpBackend/gload/controller/reviewController.java
@@ -0,0 +1,9 @@
+package meetUpBackend.gload.controller;
+
+import meetUpBackend.gload.service.reviewService;
+
+public class reviewController {
+
+    private final meetUpBackend.gload.service.reviewService reviewService;
+
+}
---
+++ src/main/java/meetUpBackend/gload/service/reviewService.java
@@ -0,0 +1,9 @@
+package meetUpBackend.gload.service;
+
+import meetUpBackend.gload.repository.reviewRepository;
+
+public class reviewService {
+
+    private final reviewRepository reviewRepository;
+
+}
