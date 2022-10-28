125453f9852c47cbc771c933334c6b3fc25323e5
Sorgfalt <yeongjun1324@gmail.com>
2022-10-29 01:30:06 +0900

    doing saving logic in review

--- src/main/java/meetUpBackend/gload/controller/reviewController.java
+++ src/main/java/meetUpBackend/gload/controller/reviewController.java
@@ -1,14 +1,40 @@
 package meetUpBackend.gload.controller;
 
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import meetUpBackend.gload.domain.Review;
 import meetUpBackend.gload.service.reviewService;
-import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class reviewController {
 
     private final reviewService reviewService;
 
+    @PostMapping("/roadMap/save")
+    public CreateReview saveReview(@RequestBody @Validated reviewRequest request) {
 
+        Review review = new Review();
+        review.setTitle(request.getTitle());
+        review.setContent(request.getContent());
+        review.setCategory(request.getCategory());
+        Long id = reviewService.join(review);
+        return new CreateReview(id);
+    }
 
+    @Data
+    static class CreateReview {
+        private Long id;
+    }
+
+    @Data
+    static class reviewRequest {
+        private String title ;
+        private String content ;
+        private String category;
+    }
 }
