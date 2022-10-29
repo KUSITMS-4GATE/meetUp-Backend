464dbc0c4368e12eb1f578c6dfb2ef605d072dac
Sorgfalt <yeongjun1324@gmail.com>
2022-10-29 19:38:47 +0900

    updated

--- src/main/java/meetUpBackend/gload/repository/roadMapRepository.java
+++ src/main/java/meetUpBackend/gload/repository/roadMapRepository.java
@@ -9,5 +9,6 @@ public class roadMapRepository {
     public interface roadMapRepository extends JpaRepository<Roadmap, Long> {
         //select m from Member m where m.name = ?
         List<Roadmap> findByName(String name); //메서드 이름을 보고 ?를 채워 넣는다.(findByName)
+
     }
 }
