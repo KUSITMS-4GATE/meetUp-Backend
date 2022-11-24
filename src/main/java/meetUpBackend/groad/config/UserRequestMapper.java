9fc188182668c18016ac266ea53f765450f6474d
dl-00-e8 <sso07012@mail.hongik.ac.kr>
2022-11-25 01:24:07 +0900

    [Fix] 로그인 domain 수정

--- src/main/java/meetUpBackend/groad/config/UserRequestMapper.java
+++ src/main/java/meetUpBackend/groad/config/UserRequestMapper.java
@@ -1,18 +1,18 @@
 package meetUpBackend.groad.config;
 
+import meetUpBackend.groad.domain.Member;
 import meetUpBackend.groad.dto.UserDto;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserRequestMapper {
-    public UserDto toDto(OAuth2User oAuth2User) {
+    public Member toDto(OAuth2User oAuth2User) {
         var attribute = oAuth2User.getAttributes();
-        return UserDto.builder()
-                .email((String) attribute.get("email"))
-                .name((String) attribute.get("name"))
-                .picture((String) attribute.get("picture"))
-                .build();
+        Member member = new Member();
+        member.setEmail((String) attribute.get("email"));
+        member.setName((String) attribute.get("name"));
+        return member;
     }
 
 }
